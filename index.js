e7791c76d2362127e580488e052cf5f65a62f205
OmEkboteCode <omekbote007@gmail.com>
2026-08-28 17:03:06 +0530

    Set up Mongoose chat model

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
-app.set("view" , path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 main()
   .then(() => console.log("Connection Successful"))
@@ -13,6 +14,15 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "Elon",
+  to: "Mark",
+  message: "I want to buy your company",
+  created_at: new Date(), //UTC
+});
+
+chat1.save().then((res) => console.log(res));
 
 app.get("/", (req, res) => {
   res.send("Working");
