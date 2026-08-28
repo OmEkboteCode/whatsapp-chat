56e69eb8e89d6a66321758bc239b85015d3264cc
OmEkboteCode <omekbote007@gmail.com>
2026-08-28 19:17:57 +0530

    Add Chat Index Route And UI

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => console.log("Connection Successful"))
@@ -15,9 +16,16 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-
 app.get("/", (req, res) => {
   res.send("Working");
+});
+
+// Index Route
+
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
 });
 
 app.listen(8080, () => {
