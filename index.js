cb2f0890467c249810195e8a12351176d3297eb5
OmEkboteCode <omekbote007@gmail.com>
2026-08-28 21:26:53 +0530

    Add Chat Creation Functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 main()
   .then(() => console.log("Connection Successful"))
@@ -30,10 +31,29 @@ app.get("/chats", async (req, res) => {
 
 //  New Route
 app.get("/chats/new", (req, res) => {
-    res.render("new.ejs")
-})
+  res.render("new.ejs");
+});
 
+//Create Route
+app.post("/chats", (req, res) => {
+  let { from, to, message } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    message: message,
+    created_at: new Date(),
+  });
 
+  newChat
+    .save()
+    .then((res) => {
+      console.log("Chat Was Saved");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
+});
 
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
