89f7ff633ebe2e85d3491be3fac9423264846631
OmEkboteCode <omekbote007@gmail.com>
2026-08-29 08:28:16 +0530

    Add Chat Edit Route And Form

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ app.post("/chats", (req, res) => {
   res.redirect("/chats");
 });
 
+
+//Edit Route
+
+app.get("/chats/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
+
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
 });
