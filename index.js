5dc347e8f94af86cbc7ecea512ee1e667f66090c
OmEkboteCode <omekbote007@gmail.com>
2026-08-29 11:07:10 +0530

    Add Chat Update Functionality

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => console.log("Connection Successful"))
@@ -25,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
-  console.log(chats);
+//   console.log(chats);
   res.render("index.ejs", { chats });
 });
 
@@ -55,14 +57,27 @@ app.post("/chats", (req, res) => {
   res.redirect("/chats");
 });
 
-
 //Edit Route
 
 app.get("/chats/:id/edit", async (req, res) => {
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs", {chat});
-})
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+// Update Route
+
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { message : newMessage } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { message: newMessage },
+    { runValidators: true, returnDocument: 'after' },
+  );
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
 
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
